8179ddf1e4862fd3258499e579b45a60fc768a25
Seif-Sallam <seif_sallam@aucegypt.edu>
2023-04-07 06:11:45 +0200

    more progress to the parser

--- semantic analyzer/parser.cpp
+++ semantic analyzer/parser.cpp
@@ -2,8 +2,308 @@
 #include <string>
 #include <fstream>
 
+// Functions of the grammar
+bool program();
+bool declaration_list();
+bool declaration_list_tail();
+bool declaration();
+bool var_declaration();
+bool var_declaration_tail();
+bool type_specifier();
+bool params();
+bool param_list();
+bool param_list_tail();
+bool param();
+bool param_tail();
+bool compound_stmt();
+bool statement_list();
+bool statement_list_tail();
+bool statement();
+bool selection_statement();
+bool selection_statement_tail();
+bool iteration_statement();
+bool assignment_statement();
+bool var();
+bool var_tail();
+bool expression();
+bool expression_tail();
+bool relop();
+bool relop_tail();
+bool additive_expression();
+bool additive_expression_tail();
+bool addop();
+bool term();
+bool term_tail();
+bool mulop();
+bool factor();
+
+
+extern int lexAnalysis(const char* outFile, const char* inputFile);
+
+struct Token {
+    std::string txt;
+    uint32_t lineNumber;
+    Token* next;
+};
+
+Token* lookAheadToken;
+std::string foundToken;
+
+
+void nextToken()
+{
+    lookAheadToken = lookAheadToken->next;
+}
+
+bool isNext(std::string currentToken)
+{
+    return (lookAheadToken->txt == currentToken);
+}
+
+bool match(std::string currentToken)
+{
+    if (lookAheadToken->txt == currentToken)
+    {
+        nextToken();
+        return true;
+    }
+    else
+    {
+        foundToken = currentToken;
+        return false;
+    }
+}
+
 int main(int argc, const char* argv[])
 {
-    std::cout << "Parser goes brrrr\n";
+    const char* intermediateFile = "testFile.txt";    
+    const char* inputFile = "input.c";
+    if (lexAnalysis(intermediateFile, inputFile)) 
+    {
+        printf("Error in syntax analysis\n");
+        remove(intermediateFile);
+        return  1;
+    }
+
+    std::ifstream inputStream(intermediateFile);
+
+    Token* list = nullptr;
+    Token* lastToken = nullptr;
+    while(!inputStream.eof())
+    {
+        std::string token;
+        uint32_t lineNumber;
+
+        inputStream >> token >> lineNumber;
+        if (token.empty() == false)
+        {
+            if (list == nullptr)
+            {
+                list = new Token{token, lineNumber, nullptr};
+                lastToken = list;
+            }
+            else 
+            {
+                lastToken->next = new Token{token, lineNumber, nullptr};
+                lastToken = lastToken->next;
+            }
+        }
+   }
+    inputStream.close();
+
+    lookAheadToken = list;
+    if (program() == false)
+    {
+        std::cerr << "Syntax error line # " << lookAheadToken->lineNumber << ", expected token (" << 
+        lookAheadToken->txt << "), but got " << "(" << foundToken << ")";
+
+    }
+
+    remove(intermediateFile);
     return 0;
 }
+
+bool program()
+{
+    return  match("Program")    &&
+            match("ID")         &&
+            declaration_list()  &&
+            statement_list();
+
+}
+
+bool declaration_list()
+{
+    return  declaration() &&
+            declaration_list_tail();
+}
+
+bool declaration_list_tail()
+{
+    if (isNext("int") || isNext("void"))
+        return declaration() && declaration_list_tail();
+    else 
+        return true;
+}
+
+bool declaration()
+{
+    return var_declaration();
+}
+
+bool var_declaration()
+{
+    return type_specifier() && match("ID") && var_declaration_tail();
+}
+
+bool var_declaration_tail()
+{
+    return match("SIMICOLON") || 
+            (match("[") 
+            && match("NUM") 
+            && match("]") 
+            && match("SIMICOLON"));
+}
+
+bool type_specifier()
+{
+    return match("int") || match("float");
+}
+
+bool params()
+{
+    return param_list() || match("void");
+}
+
+bool param_list()
+{
+    return param() && param_list_tail();
+}
+
+bool param_list_tail()
+{
+    if (isNext("COMMA"))
+        return match("COMMA") && param() && param_list_tail();
+    else
+        return true;
+}
+
+bool param()
+{
+    return type_specifier() && match("ID") && param_tail();
+}
+
+bool param_tail()
+{
+    if (isNext("LBRACKET"))
+    {
+        return match("LBRACKET") && match("RBRACKET");
+    }
+    else return true;
+}
+
+bool compound_stmt()
+{
+
+}
+
+bool statement_list()
+{
+
+}
+
+bool statement_list_tail()
+{
+
+}
+
+bool statement()
+{
+
+}
+
+bool selection_statement()
+{
+
+}
+
+bool selection_statement_tail()
+{
+
+}
+
+bool iteration_statement()
+{
+
+}
+
+bool assignment_statement()
+{
+
+}
+
+bool var()
+{
+
+}
+
+bool var_tail()
+{
+
+}
+
+bool expression()
+{
+
+}
+
+bool expression_tail()
+{
+
+}
+
+bool relop()
+{
+
+}
+
+bool relop_tail()
+{
+
+}
+
+bool additive_expression()
+{
+
+}
+
+bool additive_expression_tail()
+{
+
+}
+
+bool addop()
+{
+
+}
+
+bool term()
+{
+
+}
+
+bool term_tail()
+{
+
+}
+
+bool mulop()
+{
+
+}
+
+bool factor()
+{
+
+}
+
