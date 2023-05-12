f9f78f9990c69c604e23e7b51e24e692cbf3060b
seif-sallam <seif_sallam@aucegypt.edu>
2023-05-12 17:50:31 +0200

    Change to tabs instead of spaces

--- interpreter/parser.cpp
+++ interpreter/parser.cpp
@@ -63,9 +63,9 @@ extern int lexAnalysis(const char *outFile, const char *inputFile);
 
 struct Token
 {
-    std::string txt;
-    uint32_t lineNumber;
-    Token *next;
+	std::string txt;
+	uint32_t lineNumber;
+	Token *next;
 };
 
 Token *lookAheadToken;
@@ -73,335 +73,335 @@ std::string expectedToken;
 
 void nextToken()
 {
-    lookAheadToken = lookAheadToken->next;
+	lookAheadToken = lookAheadToken->next;
 }
 
 bool isNext(std::string currentToken)
 {
-    return (lookAheadToken->txt == currentToken);
+	return (lookAheadToken->txt == currentToken);
 }
 
 bool match(std::string _expectedToken)
 {
-    if (lookAheadToken->txt == _expectedToken)
-    {
-        nextToken();
-        return true;
-    }
-    else
-    {
-        expectedToken = _expectedToken;
-        return false;
-    }
+	if (lookAheadToken->txt == _expectedToken)
+	{
+		nextToken();
+		return true;
+	}
+	else
+	{
+		expectedToken = _expectedToken;
+		return false;
+	}
 }
 
 int main(int argc, const char *argv[])
 {
-    const char *intermediateFile = "testFile.txt";
-    const char *inputFile = argc >= 2 ? argv[1] : "input.c";
+	const char *intermediateFile = "testFile.txt";
+	const char *inputFile = argc >= 2 ? argv[1] : "input.c";
 
-    {
-        FILE* file = fopen(inputFile, "r");
-        if (file == nullptr)
-        {
-            printf("Input File not found!\n");
-            return 1;
-        }
-        fclose(file);
-    }
+	{
+		FILE* file = fopen(inputFile, "r");
+		if (file == nullptr)
+		{
+			printf("Input File not found!\n");
+			return 1;
+		}
+		fclose(file);
+	}
 
-    if (lexAnalysis(intermediateFile, inputFile))
-    {
-        printf("Error in syntax analysis\n");
-        remove(intermediateFile);
-        return 1;
-    }
+	if (lexAnalysis(intermediateFile, inputFile))
+	{
+		printf("Error in syntax analysis\n");
+		remove(intermediateFile);
+		return 1;
+	}
 
 
 
-    std::ifstream inputStream(intermediateFile);
+	std::ifstream inputStream(intermediateFile);
 
-    Token *list = nullptr;
-    Token *lastToken = nullptr;
-    while (!inputStream.eof())
-    {
-        std::string token;
-        uint32_t lineNumber;
+	Token *list = nullptr;
+	Token *lastToken = nullptr;
+	while (!inputStream.eof())
+	{
+		std::string token;
+		uint32_t lineNumber;
 
-        inputStream >> token >> lineNumber;
-        if (token.empty() == false)
-        {
-            if (list == nullptr)
-            {
-                list = new Token{token, lineNumber, nullptr};
-                lastToken = list;
-            }
-            else
-            {
-                lastToken->next = new Token{token, lineNumber, nullptr};
-                lastToken = lastToken->next;
-            }
-        }
-    }
-    inputStream.close();
+		inputStream >> token >> lineNumber;
+		if (token.empty() == false)
+		{
+			if (list == nullptr)
+			{
+				list = new Token{token, lineNumber, nullptr};
+				lastToken = list;
+			}
+			else
+			{
+				lastToken->next = new Token{token, lineNumber, nullptr};
+				lastToken = lastToken->next;
+			}
+		}
+	}
+	inputStream.close();
 
-    lookAheadToken = list;
-    if (program() == false)
-    {
-        std::cerr << "Syntax error line # " << lookAheadToken->lineNumber << ", expected token (" << expectedToken << "), but got "
-                  << "(" << lookAheadToken->txt << ")\n";
-    }
-    else
-        std::cout << "Parsing has completed successfully!\n";
+	lookAheadToken = list;
+	if (program() == false)
+	{
+		std::cerr << "Syntax error line # " << lookAheadToken->lineNumber << ", expected token (" << expectedToken << "), but got "
+				  << "(" << lookAheadToken->txt << ")\n";
+	}
+	else
+		std::cout << "Parsing has completed successfully!\n";
 
-    remove(intermediateFile);
+	remove(intermediateFile);
 
-    return 0;
+	return 0;
 }
 using namespace std;
 
 bool debug(int x = 0)
 {
-    cout << "Hello: " << x << "\n";
-    return true;
+	cout << "Hello: " << x << "\n";
+	return true;
 }
 
 bool program()
 {
-    return type_specifier() && match("main") && match(LPAREN) && params() && match(RPAREN) && match(LBRACE) && declaration_list() && statement_list() && match(RBRACE);
+	return type_specifier() && match("main") && match(LPAREN) && params() && match(RPAREN) && match(LBRACE) && declaration_list() && statement_list() && match(RBRACE);
 }
 
 bool declaration_list()
 {
-    return declaration() &&
-           declaration_list_tail();
+	return declaration() &&
+		   declaration_list_tail();
 }
 
 bool declaration_list_tail()
 {
-    if (isNext("int") || isNext("float"))
-        return declaration() && declaration_list_tail();
-    else
-        return true;
+	if (isNext("int") || isNext("float"))
+		return declaration() && declaration_list_tail();
+	else
+		return true;
 }
 
 bool declaration()
 {
-    return var_declaration();
+	return var_declaration();
 }
 
 bool var_declaration()
 {
-    return type_specifier() && match(ID) && var_declaration_tail();
+	return type_specifier() && match(ID) && var_declaration_tail();
 }
 
 bool var_declaration_tail()
 {
-    return (match(LBRACKET) && match(NUM) && match(RBRACKET) && match(SEMICOLON)) || match(SEMICOLON);
+	return (match(LBRACKET) && match(NUM) && match(RBRACKET) && match(SEMICOLON)) || match(SEMICOLON);
 }
 
 bool type_specifier()
 {
-    return match("int") ||
-           match("float");
+	return match("int") ||
+		   match("float");
 }
 
 bool params()
 {
-    if (isNext("void"))
-    {
-        return match("void");
-    }
-    else
-        return param_list();
+	if (isNext("void"))
+	{
+		return match("void");
+	}
+	else
+		return param_list();
 }
 
 bool param_list()
 {
-    return param() && param_list_tail();
+	return param() && param_list_tail();
 }
 
 bool param_list_tail()
 {
-    if (isNext(COMMA))
-        return match(COMMA) && param() && param_list_tail();
-    else
-        return true;
+	if (isNext(COMMA))
+		return match(COMMA) && param() && param_list_tail();
+	else
+		return true;
 }
 
 bool param()
 {
-    return type_specifier() && match(ID) && param_tail();
+	return type_specifier() && match(ID) && param_tail();
 }
 
 bool param_tail()
 {
-    if (isNext(LBRACKET))
-    {
-        return match(LBRACKET) && match(RBRACKET);
-    }
-    else
-        return true;
+	if (isNext(LBRACKET))
+	{
+		return match(LBRACKET) && match(RBRACKET);
+	}
+	else
+		return true;
 }
 
 bool compound_stmt()
 {
-    return match(LBRACE) && statement_list() && match(RBRACE);
+	return match(LBRACE) && statement_list() && match(RBRACE);
 }
 
 bool statement_list()
 {
-    return statement_list_tail();
+	return statement_list_tail();
 }
 
 bool statement_list_tail()
 {
-    if (isNext(ID) || isNext("if") || isNext("while") || isNext(LBRACE))
-        return statement() && statement_list_tail();
-    else
-        return true;
+	if (isNext(ID) || isNext("if") || isNext("while") || isNext(LBRACE))
+		return statement() && statement_list_tail();
+	else
+		return true;
 }
 
 bool statement()
 {
-    return selection_statement() ||
-           iteration_statement() ||
-           assignment_statement() ||
-           compound_stmt();
+	return selection_statement() ||
+		   iteration_statement() ||
+		   assignment_statement() ||
+		   compound_stmt();
 }
 
 bool selection_statement()
 {
-    return match("if") &&
-           match(LPAREN) &&
-           expression() &&
-           match(RPAREN) &&
-           statement() &&
-           selection_statement_tail();
+	return match("if") &&
+		   match(LPAREN) &&
+		   expression() &&
+		   match(RPAREN) &&
+		   statement() &&
+		   selection_statement_tail();
 }
 
 bool selection_statement_tail()
 {
-    if (isNext("else"))
-        return match("else") && statement();
-    else
-        return true;
+	if (isNext("else"))
+		return match("else") && statement();
+	else
+		return true;
 }
 
 bool iteration_statement()
 {
-    return match("while") &&
-           match(LPAREN) &&
-           expression() &&
-           match(RPAREN) &&
-           statement();
+	return match("while") &&
+		   match(LPAREN) &&
+		   expression() &&
+		   match(RPAREN) &&
+		   statement();
 }
 
 bool assignment_statement()
 {
-    return var() &&
-           match(ASSIGN) &&
-           expression() && match(SEMICOLON);
+	return var() &&
+		   match(ASSIGN) &&
+		   expression() && match(SEMICOLON);
 }
 
 bool var()
 {
-    return match(ID) &&
-           var_tail();
+	return match(ID) &&
+		   var_tail();
 }
 
 bool var_tail()
 {
-    if (isNext(LBRACKET))
-        return match(LBRACKET) && expression() && match(RBRACKET);
-    else
-        return true;
+	if (isNext(LBRACKET))
+		return match(LBRACKET) && expression() && match(RBRACKET);
+	else
+		return true;
 }
 
 bool expression()
 {
-    return additive_expression() && expression_tail();
+	return additive_expression() && expression_tail();
 }
 
 bool expression_tail()
 {
-    if (isNext(LT) || isNext(GT) || isNext(EQ) || isNext(NEQ))
-        return relop() && additive_expression() && expression_tail();
-    else
-        return true;
+	if (isNext(LT) || isNext(GT) || isNext(EQ) || isNext(NEQ))
+		return relop() && additive_expression() && expression_tail();
+	else
+		return true;
 }
 
 bool relop()
 {
-    if (isNext(LT))
-        return (match(LT) && relop_tail());
-    else if (isNext(GT))
-        return (match(GT) && relop_tail());
-    else if (isNext(EQ))
-        return match(EQ);
-    else
-        return match(NEQ);
+	if (isNext(LT))
+		return (match(LT) && relop_tail());
+	else if (isNext(GT))
+		return (match(GT) && relop_tail());
+	else if (isNext(EQ))
+		return match(EQ);
+	else
+		return match(NEQ);
 }
 
 bool relop_tail()
 {
-    if (isNext(ASSIGN))
-        return match(ASSIGN);
-    return true;
+	if (isNext(ASSIGN))
+		return match(ASSIGN);
+	return true;
 }
 
 bool additive_expression()
 {
-    return term() && additive_expression_tail();
+	return term() && additive_expression_tail();
 }
 
 bool additive_expression_tail()
 {
-    if (isNext(PLUS) || isNext(MINUS))
-        return addop() && term() && additive_expression_tail();
+	if (isNext(PLUS) || isNext(MINUS))
+		return addop() && term() && additive_expression_tail();
 
-    return true;
+	return true;
 }
 
 bool addop()
 {
-    if (isNext(PLUS))
-        return (match(PLUS));
-    else
-        return (match(MINUS));
+	if (isNext(PLUS))
+		return (match(PLUS));
+	else
+		return (match(MINUS));
 }
 
 bool term()
 {
-    return factor() && term_tail();
+	return factor() && term_tail();
 }
 
 bool term_tail()
 {
-    if (isNext(MULT) || isNext(DIV))
-        return mulop() && factor() && term_tail();
-    else
-        return true;
+	if (isNext(MULT) || isNext(DIV))
+		return mulop() && factor() && term_tail();
+	else
+		return true;
 }
 
 bool mulop()
 {
-    if (isNext(MULT))
-        return (match(MULT));
-    else
-        return (match(DIV));
+	if (isNext(MULT))
+		return (match(MULT));
+	else
+		return (match(DIV));
 }
 
 bool factor()
 {
-    if (isNext(NUM))
-        return match(NUM);
-    else if (isNext(ID))
-        return match(ID);
-    else
-        return match(LPAREN) && expression() && match(RPAREN);
+	if (isNext(NUM))
+		return match(NUM);
+	else if (isNext(ID))
+		return match(ID);
+	else
+		return match(LPAREN) && expression() && match(RPAREN);
 
-    // return match(NUM) ||
-    //        match(ID) ||
-    //        (match(LPAREN) && expression() && match(RPAREN));
+	// return match(NUM) ||
+	//        match(ID) ||
+	//        (match(LPAREN) && expression() && match(RPAREN));
 }
