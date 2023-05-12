bf81c7464a816968c280ba7a533146b87b648c3f
seif-sallam <seif_sallam@aucegypt.edu>
2023-05-12 17:49:23 +0200

    restructure

--- interpreter/parser.cpp
+++ interpreter/parser.cpp
@@ -1,4 +1,170 @@
-#include "parser.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#define PLUS "PLUS"
+#define MINUS "MINUS"
+#define ASSIGN "ASSIGN"
+#define MULT "MULT"
+#define DIV "DIV "
+#define LT "LT"
+#define LEQ "LEQ"
+#define GT "GT"
+#define GEQ "GEQ"
+#define EQ "EQ"
+#define NEQ "NEQ"
+#define LBRACKET "LBRACKET"
+#define RBRACKET "RBRACKET"
+#define LPAREN "LPAREN"
+#define RPAREN "RPAREN"
+#define LBRACE "LBRACE"
+#define RBRACE "RBRACE"
+#define SEMICOLON "SEMICOLON"
+#define COMMA "COMMA"
+#define ID "ID"
+#define NUM "NUM"
+
+// Functions of the grammar
+bool program();
+bool declaration_list();
+bool declaration_list_tail();
+bool declaration();
+bool var_declaration();
+bool var_declaration_tail();
+bool type_specifier();
+bool params();
+bool param_list();
+bool param_list_tail();
+bool param();
+bool param_tail();
+bool compound_stmt();
+bool statement_list();
+bool statement_list_tail();
+bool statement();
+bool selection_statement();
+bool selection_statement_tail();
+bool iteration_statement();
+bool assignment_statement();
+bool var();
+bool var_tail();
+bool expression();
+bool expression_tail();
+bool relop();
+bool relop_tail();
+bool additive_expression();
+bool additive_expression_tail();
+bool addop();
+bool term();
+bool term_tail();
+bool mulop();
+bool factor();
+
+extern int lexAnalysis(const char *outFile, const char *inputFile);
+
+struct Token
+{
+    std::string txt;
+    uint32_t lineNumber;
+    Token *next;
+};
+
+Token *lookAheadToken;
+std::string expectedToken;
+
+void nextToken()
+{
+    lookAheadToken = lookAheadToken->next;
+}
+
+bool isNext(std::string currentToken)
+{
+    return (lookAheadToken->txt == currentToken);
+}
+
+bool match(std::string _expectedToken)
+{
+    if (lookAheadToken->txt == _expectedToken)
+    {
+        nextToken();
+        return true;
+    }
+    else
+    {
+        expectedToken = _expectedToken;
+        return false;
+    }
+}
+
+int main(int argc, const char *argv[])
+{
+    const char *intermediateFile = "testFile.txt";
+    const char *inputFile = argc >= 2 ? argv[1] : "input.c";
+
+    {
+        FILE* file = fopen(inputFile, "r");
+        if (file == nullptr)
+        {
+            printf("Input File not found!\n");
+            return 1;
+        }
+        fclose(file);
+    }
+
+    if (lexAnalysis(intermediateFile, inputFile))
+    {
+        printf("Error in syntax analysis\n");
+        remove(intermediateFile);
+        return 1;
+    }
+
+
+
+    std::ifstream inputStream(intermediateFile);
+
+    Token *list = nullptr;
+    Token *lastToken = nullptr;
+    while (!inputStream.eof())
+    {
+        std::string token;
+        uint32_t lineNumber;
+
+        inputStream >> token >> lineNumber;
+        if (token.empty() == false)
+        {
+            if (list == nullptr)
+            {
+                list = new Token{token, lineNumber, nullptr};
+                lastToken = list;
+            }
+            else
+            {
+                lastToken->next = new Token{token, lineNumber, nullptr};
+                lastToken = lastToken->next;
+            }
+        }
+    }
+    inputStream.close();
+
+    lookAheadToken = list;
+    if (program() == false)
+    {
+        std::cerr << "Syntax error line # " << lookAheadToken->lineNumber << ", expected token (" << expectedToken << "), but got "
+                  << "(" << lookAheadToken->txt << ")\n";
+    }
+    else
+        std::cout << "Parsing has completed successfully!\n";
+
+    remove(intermediateFile);
+
+    return 0;
+}
+using namespace std;
+
+bool debug(int x = 0)
+{
+    cout << "Hello: " << x << "\n";
+    return true;
+}
 
 bool program()
 {
@@ -234,4 +400,8 @@ bool factor()
         return match(ID);
     else
         return match(LPAREN) && expression() && match(RPAREN);
+
+    // return match(NUM) ||
+    //        match(ID) ||
+    //        (match(LPAREN) && expression() && match(RPAREN));
 }
